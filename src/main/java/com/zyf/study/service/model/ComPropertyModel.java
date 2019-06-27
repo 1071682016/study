da696535697dd74d6d17bd3a5997e739164ddfb5
zyf <1071682016@qq.com>
2019-06-27 14:28:56 +0800

    Merge branch 'master' of https://github.com/1071682016/study
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/zyf/study/service/model/ComPropertyModel.java
@@ -0,0 +1,30 @@
+package com.zyf.study.service.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 012
+ * Date: 2019/6/27
+ * Time: 10:27
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class ComPropertyModel  {
+    private int num;
+    private String name;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
